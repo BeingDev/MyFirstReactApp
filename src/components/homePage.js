c975dd13fb8865ed565af5fd6843c55fb2fe8525
V G <vidwan_20@yahoo.com>
2015-08-14 14:48:45 -0400

    completed 'Creating Components with React" module

---
+++ src/components/homePage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+var React = require('react');
+
+var Home = React.createClass({
+	render: function () {
+		return(
+			<div className="jumbotron">
+				<h1>Pluralsight Administration</h1>
+				<p>asdfasdfasdf</p>
+			</div>
+		);
+	}
+});
+
+module.exports = Home;
