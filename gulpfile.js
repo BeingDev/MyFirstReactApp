aa8e8c41caff6deecec189931cf6b7c447a21bd4
V G <vidwan_20@yahoo.com>
2015-08-14 11:22:24 -0400

    minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,31 @@
-'use strict';
+"use strict";
 
 var gulp = require('gulp');
-var connect = require('gulp-connect');
-var open = require('gulp-open');
-
-var browserify = require('browserify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream');
-var concat = require('gulp-concat');
-var eslint = require('gulp-eslint');
+var connect = require('gulp-connect'); //Runs a local dev server
+var open = require('gulp-open'); //Open a URL in a web browser
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify');  // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
+var concat = require('gulp-concat'); //Concatenates files
+var lint = require('gulp-eslint'); //Lint JS files, including JSX
 
 var config = {
 	port: 9005,
-	devBaseUrl: 'http://locahost',
-	path: {
+	devBaseUrl: 'http://localhost',
+	paths: {
 		html: './src/*.html',
 		js: './src/**/*.js',
-		dist: './dist',
-		mainJs: './src/main.js',
 		css: [
-			'node_modules/bootstrap/dist/css/bootstrap.min.css',
-			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
-		],
+      		'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+    	],
+		dist: './dist',
+		mainJs: './src/main.js'
 	}
-};
+}
 
-
-gulp.task('connect', function () {
+//Start a local development server
+gulp.task('connect', function() {
 	connect.server({
 		root: ['dist'],
 		port: config.port,
@@ -35,43 +34,42 @@ gulp.task('connect', function () {
 	});
 });
 
-
-gulp.task('open', ['connect'], function () {
+gulp.task('open', ['connect'], function() {
 	gulp.src('dist/index.html')
-		.pipe(open('', { url: config.devBaseUrl + ':' + config.port + '/' }));
+		.pipe(open('', { url: config.devBaseUrl + ':' + config.port + '/'}));
 });
 
-gulp.task('html', function () {
-	gulp.src(config.path.html)
-		.pipe(gulp.dest(config.path.dist))
+gulp.task('html', function() {
+	gulp.src(config.paths.html)
+		.pipe(gulp.dest(config.paths.dist))
 		.pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
-	gulp.watch(config.path.html, ['html']);
-	gulp.watch(config.path.js, ['js', 'lint']);
-});
-
-gulp.task('js', function () {
-	browserify(config.path.mainJs)
+gulp.task('js', function() {
+	browserify(config.paths.mainJs)
 		.transform(reactify)
 		.bundle()
 		.on('error', console.error.bind(console))
 		.pipe(source('bundle.js'))
-		.pipe(gulp.dest(config.path.dist + '/scripts'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
 		.pipe(connect.reload());
 });
 
-gulp.task('css', function () {
-	gulp.src(config.path.css)
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
 		.pipe(concat('bundle.css'))
-		.pipe(gulp.dest(config.path.dist + '/css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('lint', function () {
-	return gulp.src(config.path.js)
-		.pipe(eslint({ config: 'eslint.config.json' }))
-		.pipe(eslint.format());
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint({config: 'eslint.config.json'}))
+		.pipe(lint.format());
 });
 
-gulp.task('default', ['html', 'css', 'js', 'lint', 'open', 'watch']);
+gulp.task('watch', function() {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
